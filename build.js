826ad5ac15c2080982b98420e0ca946af82d2663
Linus Metzler <hi@linusmetzler.me>
2017-06-16 14:31:59 +0200

    inline CSS

--- build.js
+++ build.js
@@ -12,42 +12,41 @@ glob('configs/*.json', (err0, files) => {
         fs.readJson(file, (err1, config) => {
             if (err1) console.error(err1);
 
-            const outFile = path.join(__dirname, '/public/', `${path.basename(file, '.json')}.html`);
-            const outCss = path.join(__dirname, '/public/', `${path.basename(file, '.json')}.css`);
+            const template = path.join(__dirname, `templates/${config.template}.html`);
+            const html = path.join(__dirname, '/public/', `${path.basename(file, '.json')}.html`);
 
             let data = '';
             mu.clearCache();
-            mu.compileAndRender(path.join(__dirname, `templates/${config.template}.html`), config)
+            mu.compileAndRender(template, config)
                 .on('data', (d) => {
                     data += d.toString();
                 }).on('end', () => {
-                    try {
-                        data = minify(data, {
-                            collapseWhitespace: true,
-                            collapseBooleanAttributes: true,
-                            collapseInlineTagWhitespace: true,
-                            decodeEntities: true,
-                            minifyCSS: true,
-                            minifyJS: true,
-                            minifyURLs: true,
-                            removeComments: true,
-                            removeEmptyAttributes: true,
-                            removeOptionalTags: true,
-                            removeRedundantAttributes: true,
-                            removeScriptTypeAttributes: true,
-                            removeStyleLinkTypeAttributes: true,
-                            useShortDoctype: true,
-                        });
-                    } catch (err) {
-                        console.error(`Minification failed for ${file}`);
-                    }
                     const bootstrap = fs.readFileSync(path.join(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.min.css'), 'utf8');
-                    purifycss(data, bootstrap, {
-                        output: outCss,
-                        minify: true,
+                    purifycss(data, bootstrap, { minify: true }, (css) => {
+                        data = data.replace('<style></style>', `<style>${css}</style>`);
+                        try {
+                            data = minify(data, {
+                                collapseWhitespace: true,
+                                collapseBooleanAttributes: true,
+                                collapseInlineTagWhitespace: true,
+                                decodeEntities: true,
+                                minifyCSS: true,
+                                minifyJS: true,
+                                minifyURLs: true,
+                                removeComments: true,
+                                removeEmptyAttributes: true,
+                                removeOptionalTags: true,
+                                removeRedundantAttributes: true,
+                                removeScriptTypeAttributes: true,
+                                removeStyleLinkTypeAttributes: true,
+                                useShortDoctype: true,
+                            });
+                        } catch (err) {
+                            console.error(`Minification failed for ${file}`);
+                        }
+                        fs.writeFile(html, data);
+                        console.info(`Success for ${file}`);
                     });
-                    fs.writeFile(outFile, data);
-                    console.info(`Success for ${file}`);
                 });
         });
     }
