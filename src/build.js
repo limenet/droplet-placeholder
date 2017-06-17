a0c6c3beb4ee8bb81c0be7f9ad84b64c807481d1
Linus Metzler <hi@linusmetzler.me>
2017-06-17 21:07:57 +0200

    more refactoring

--- src/build.js
+++ src/build.js
@@ -11,24 +11,8 @@ const directories = {
     configs: 'configs',
 };
 
-function parseConfig(file, c) {
-    const images = {
-        limenetch: helpers.image('https://s3.amazonaws.com/limenet-logo-img/v2/full-transparent-height20.png'),
-        digitalocean: helpers.image('https://s3.amazonaws.com/multisite-misc-assets/do-hosted-by.png'),
-        faCode: helpers.image('https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/black/svg/code.svg', 'image/svg+xml'),
-    };
-
-    const config = c;
+function renderTemplate(file, config) {
     const basename = path.basename(file, '.json');
-
-    Object.entries(images).forEach(([key, value]) => {
-        config[key] = value;
-    });
-
-    if ('gravatar' in config) {
-        config.gravatar = helpers.image(`https://www.gravatar.com/avatar/${config.gravatar}?rating=G&size=256`);
-    }
-
     const template = path.join(directories.templates, `${config.template}.html`);
     const html = path.join(directories.output, `${basename}.html`);
 
@@ -50,6 +34,27 @@ function parseConfig(file, c) {
                 log.info(basename);
             });
         });
+}
+
+
+function parseConfig(file, c) {
+    const images = {
+        limenetch: helpers.image('https://s3.amazonaws.com/limenet-logo-img/v2/full-transparent-height20.png'),
+        digitalocean: helpers.image('https://s3.amazonaws.com/multisite-misc-assets/do-hosted-by.png'),
+        faCode: helpers.image('https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/black/svg/code.svg', 'image/svg+xml'),
+    };
+
+    const config = c;
+
+    Object.entries(images).forEach(([key, value]) => {
+        config[key] = value;
+    });
+
+    if ('gravatar' in config) {
+        config.gravatar = helpers.image(`https://www.gravatar.com/avatar/${config.gravatar}?rating=G&size=256`);
+    }
+
+    renderTemplate(file, config);
 }
 
 glob(path.join(directories.configs, '*.json'), (err0, files) => {
