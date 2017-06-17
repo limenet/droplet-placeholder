930308a49bbaff1c56df983d28c8a4f2fd43daaf
Linus Metzler <hi@linusmetzler.me>
2017-06-17 15:51:57 +0200

    more tests

--- test/test.js
+++ test/test.js
@@ -3,22 +3,19 @@ const assert = require('assert');
 const helpers = require('../src/helpers');
 const mime = require('mime-types');
 const fs = require('fs-extra');
+const cssParser = require('css');
 
-describe('Images', () => {
+describe('image()', () => {
     const url = 'https://s3.amazonaws.com/limenet-logo-img/v2/full-transparent-height20.png';
 
     it('caches the response', () => {
         fs.emptyDirSync(helpers.cacheDir);
 
-        assert(helpers.image(url) !== '');
+        assert.notEqual(helpers.image(url), '');
 
         fs.readdir(helpers.cacheDir, (err, items) => {
             assert(items.length > 1);
         });
-    });
-
-    it('returns something', () => {
-        assert(helpers.image(url) !== '');
     });
 
     it('respects custom MIME type', () => {
@@ -26,21 +23,21 @@ describe('Images', () => {
         const regex = 'data:(.*);';
         const image = helpers.image(url, mimeType).toString();
         const match = image.match(regex);
-        assert(match[1] === mimeType);
+        assert.equal(match[1], mimeType);
     });
 
     it('contains MIME type', () => {
         const regex = 'data:(.*);';
         const image = helpers.image(url).toString();
         const match = image.match(regex);
-        assert(mime.extension(match[1]) !== '');
+        assert.notEqual(mime.extension(match[1]), '');
     });
 
     it('contains base64 header', () => {
         const regex = '^data:(.*);(.*),';
         const image = helpers.image(url).toString();
         const match = image.match(regex);
-        assert(match[2] === 'base64');
+        assert.equal(match[2], 'base64');
     });
 
     it('is in base64', () => {
@@ -51,6 +48,21 @@ describe('Images', () => {
     });
 
     it('fails gracefully', () => {
-        assert(helpers.image('https://example.com/404.jpg') === '');
+        assert.equal(helpers.image('https://example.com/404.jpg'), '');
+    });
+});
+
+describe('css()', () => {
+    it('returns valid CSS', () => {
+        assert.doesNotThrow(() => { cssParser.parse(helpers.css('')); });
+    });
+
+    it('strips comments and @font-face', () => {
+        assert.doesNotThrow(() => {
+            Object.values(cssParser.parse(helpers.css('')).stylesheet.rules).forEach((rule) => {
+                assert.notEqual(rule.type, 'comment');
+                assert.notEqual(rule.type, 'font-face');
+            });
+        });
     });
 });
