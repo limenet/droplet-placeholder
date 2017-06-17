aa90d8c90199390146c58205556ed9bc5c5bd1c7
Linus Metzler <hi@linusmetzler.me>
2017-06-17 21:14:09 +0200

    refactoring 3

--- src/build.js
+++ src/build.js
@@ -11,6 +11,18 @@ const directories = {
     configs: 'configs',
 };
 
+function outputHtml(file, data) {
+    const basename = path.basename(file, '.html');
+    let d = data;
+    d = d.replace('<style></style>', `<style>${helpers.css(d)}</style>`);
+    d = helpers.minifyHtml(d);
+    fs.writeFile(file, d, (err2) => {
+        if (err2) log.error(err2);
+
+        log.info(basename);
+    });
+}
+
 function renderTemplate(file, config) {
     const basename = path.basename(file, '.json');
     const template = path.join(directories.templates, `${config.template}.html`);
@@ -22,34 +34,28 @@ function renderTemplate(file, config) {
             data += d.toString();
         })
         .on('end', () => {
-            data = data.replace('<style></style>', `<style>${helpers.css(data)}</style>`);
-            data = helpers.minifyHtml(data);
-            fs.writeFile(out, data, (err2) => {
-                if (err2) log.error(err2);
-
-                log.info(basename);
-            });
+            outputHtml(out, data);
         });
 }
 
-function parseConfig(file, c) {
+function parseConfig(file, config) {
     const images = {
         limenetch: helpers.image('https://s3.amazonaws.com/limenet-logo-img/v2/full-transparent-height20.png'),
         digitalocean: helpers.image('https://s3.amazonaws.com/multisite-misc-assets/do-hosted-by.png'),
         faCode: helpers.image('https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/black/svg/code.svg', 'image/svg+xml'),
     };
 
-    const config = c;
+    const c = config;
 
     Object.entries(images).forEach(([key, value]) => {
-        config[key] = value;
+        c[key] = value;
     });
 
-    if ('gravatar' in config) {
-        config.gravatar = helpers.image(`https://www.gravatar.com/avatar/${config.gravatar}?rating=G&size=256`);
+    if ('gravatar' in c) {
+        c.gravatar = helpers.image(`https://www.gravatar.com/avatar/${c.gravatar}?rating=G&size=256`);
     }
 
-    renderTemplate(file, config);
+    renderTemplate(file, c);
 }
 
 glob(path.join(directories.configs, '*.json'), (err0, files) => {
